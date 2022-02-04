f408608dcead059b1f2e835ba41426e303cd3cb8
a-fotherby <af606@cam.ac.uk>
2022-02-04 10:33:44 +0000

    refactoring and updates

--- TecplotTools.py
+++ TecplotTools.py
@@ -5,8 +5,9 @@ import matplotlib.cm as cm
 import os
 from glob import glob
 
+
 def read_tecplot(file_cat, file_num):
-    file_name = '{}{}.tec'.format(file_cat, file_num)
+    file_name = f'{file_cat}{file_num}.tec'
     with open(file_name) as f:
         f.readline()
         header_line = f.readline()
@@ -14,10 +15,10 @@ def read_tecplot(file_cat, file_num):
         column_headers = column_headers[2:]
         for i in column_headers:
             column_headers[column_headers.index(i)] = i.replace('"', '')
-                
-        df = pd.read_csv(file_name, sep=' ', skipinitialspace=True, skiprows=[0,1,2], names=column_headers)
-        
+
+        df = pd.read_csv(file_name, sep=' ', skipinitialspace=True, skiprows=[0, 1, 2], names=column_headers)
         return df, column_headers
+
 
 def tecplot_2d(data_frame, scalar_name, vmin, vmax):
     z = data_frame.pivot('Y', 'X', scalar_name)
@@ -25,17 +26,20 @@ def tecplot_2d(data_frame, scalar_name, vmin, vmax):
     levels = np.linspace(vmin, vmax, 16)
     CS = plt.contourf(x, y, z, levels=levels, cmap=cm.viridis, extend='both')
 
-    colorbar = plt.colorbar(CS)
+    colour_bar = plt.colorbar(CS)
     plt.xlabel('y / m')
     plt.ylabel('x / m')
     plt.show()
-    
-def initialise1D(file_cat, lims=(-1,1)):
+
+
+def initialise1D(file_cat):
     df, column_headers = read_tecplot(file_cat, 1)
-    fig, ax = plt.subplots(figsize=(9,6))
+    fig, ax = plt.subplots(figsize=(9, 6))
     line, = ax.plot(df['X'], np.zeros_like(df['X']))
+
     return fig, ax, line
-    
+
+
 def data_cats(directory):
     os.chdir(directory)
     f_list = glob('*.tec')
@@ -45,34 +49,61 @@ def data_cats(directory):
     output_total = len(f_list) / len(f_set)
     return f_set, int(output_total)
 
+
+def box_plot(file_cat, plot_var, max_time):
+    """Plot out the time series for the file category in a box model (i.e. 0D CT model)."""
+    import numpy as np
+
+    series = np.empty(max_time)
+    try:
+        for i in np.arange(0, max_time):
+            df, column_headers = read_tecplot(file_cat, i+1)
+            series[i] = df[plot_var]
+    except KeyError:
+        return
+
+    return series
+
+
+def initialise_box(file_cat):
+    df, column_headers = read_tecplot(file_cat, 1)
+    fig, ax = plt.subplots(figsize=(9, 6))
+    ax.scatter(df['X'], np.zeros_like(df['X']))
+
+    return fig, ax
+
+
 def time_nav(file_cat, time, plot_var, max_time):
     df, column_headers = read_tecplot(file_cat, time)
     vmin, vmax = plot_var_range(max_time, file_cat, plot_var)
     tecplot_2d(df, plot_var, vmin, vmax)
-    
+
+
 def time_nav_1d(file_cat, time, plot_var, max_time):
     df, column_headers = read_tecplot(file_cat, time)
     lims = plot_var_range(max_time, file_cat, plot_var)
-    tecplot_1d(df, plot_var, lims)    
-    
+    tecplot_1d(df, plot_var, lims)
+
+
 def plot_var_range(max_time, file_cat, plot_var):
     min_list = []
     max_list = []
-    for t in range(1, max_time+1):
+    for t in range(1, max_time + 1):
         df = read_tecplot(file_cat, t)[0]
         s = df.loc[:, plot_var]
         min_list.append(s.nsmallest(1).values)
-        max_list.append(s.nlargest(1).values)    
-        
+        max_list.append(s.nlargest(1).values)
+
     lower = np.amin(min_list)
     upper = np.amax(max_list)
-    
+
     return lower, upper
+
 
 def import_time_series(file_name):
     with open(file_name) as f:
         f.readline()
-        header_line = f.readline()    
+        header_line = f.readline()
     column_headers = header_line[11:]
     column_headers = column_headers.replace("'", "")
     column_headers = column_headers.replace('"', "")
@@ -82,12 +113,39 @@ def import_time_series(file_name):
     column_headers = column_headers.rstrip(',')
     column_headers = column_headers.split(',')
     print(column_headers)
-    
-    df = pd.read_csv(file_name, engine='python', sep='\s+', skiprows=[0,1]) 
-    df.columns=column_headers
+
+    df = pd.read_csv(file_name, engine='python', sep='\s+', skiprows=[0, 1])
+    df.columns = column_headers
     return df, column_headers
+
 
 def breakthrough(time, plot_var, data_frame):
     fig, ax = plt.subplots()
     ax.plot(time, data_frame.loc[:, plot_var])
     return fig, ax
+
+def read_times(path):
+    """return a dictionary of lines in a file, with the values as the line numbers.
+
+    will ignore any commented lines in the ct input file, but will still count their line number,
+    so line numbers in dictionary will map to the true line number in the file.
+    """
+    import re
+    
+    with open(path, 'r') as f:
+        for line_num, line in enumerate(f):
+            # input files edited on unix systems have newline characters that must be stripped.
+            # also strip any trailing whitespace.
+            if line.startswith('spatial_profile'):
+                line = line.rstrip('\n ')
+                line = re.split('\s+', line)
+                line.pop(0)
+                break
+            else:
+                pass
+
+        f.close()
+        
+        line = [float(x) for x in line]
+    return line
+
