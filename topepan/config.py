01ddff331362427806cc93384e074b92310224a2
a-fotherby <af606@cam.ac.uk>
2022-06-15 11:13:02 +0100

    restructure

---
+++ topepan/config.py
@@ -0,0 +1,5 @@
+import Pathlib
+
+# Put the absolute path to your CrunchTope input files in these variables.
+box_path = Path('/Users/angus/Dropbox/sukinda/models/box_model_dev/box.in'
+column_path = '/path/to/your/outputs/'
---
+++ topepan/geochem.py
@@ -0,0 +1,3 @@
+def delta(rare, common, standard):
+    delta = (((rare / common) / tope) - 1) * 1000
+    return delta
