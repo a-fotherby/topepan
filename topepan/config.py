c95a2ca2b5510164038c0b867ae9bd52589f49cc
Angus Fotherby <af606@cam.ac.uk>
2023-01-18 14:18:54 +0000

    for consistancy

--- topepan/config.py
+++ topepan/config.py
@@ -4,7 +4,7 @@ import pathlib as pl
 home = pl.Path.home()
 
 # Path for box.ipynb
-box_path = home / 'work' / 'hydrothermal_alteration' / 'model' / 'box_equil' / 'boxModel.in'
+box_path = home / 'work' / 'hydrothermal_alteration' / 'model' / 'box_equil' / 'box.in'
 
 # Path for column.ipynb
 column_path = home / 'work' / 'hydrothermal_alteration' / 'model' / 'model' / 'burn.in'
