711e5cb35d6e9ddf9190081ae64fa3c3e376ec8c
a-fotherby <af606@cam.ac.uk>
2023-01-18 12:17:01 +0000

    update for more system agnostic paths

--- topepan/config.py
+++ topepan/config.py
@@ -1,10 +1,11 @@
-import pathlib as p
+import sys
+import pathlib as pl
 
-# Put the absolute path to your CrunchTope input files in these variables.
-# Path for box.ipynb.
-box_path = p.Path('~/work/tom_Project/model/basalt_5-1.in')
+home = pl.Path.home()
 
-# Path for column.ipynb.
-#column_path = p.Path('/home/angus/Dropbox/sukinda/models/1D_models/high_Cr_13-06-22/column.in')
-#column_path = p.Path('/home/angus/work/eoin/Ex 24/EI_A.in')
-column_path = p.Path('/home/angus/work/eoin/EI_A.in')
+
+# Path for box.ipynb
+box_path = home / 'work' / 'crunch_models' / 'temp_test' / 'boxModel.in'
+
+# Path for column.ipynb
+column_path = home / 'work' / 'crunch_models' / 'calcium_column' / 'flow_field.in'
